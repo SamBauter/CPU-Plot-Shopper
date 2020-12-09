0385bf4b1201957698e8ee3450506488ea2ba002
Sam <samuelbauter@gmail.com>
2020-12-09 11:07:32 -0700

    Fixed empty qs bug

--- Capstone/main_app/views.py
+++ Capstone/main_app/views.py
@@ -21,125 +21,150 @@ def dashboard(request):
 
 def monitor_graph(request):
     f = MonitorFilter(request.GET, queryset=MonitorSpecs.objects.filter(price__isnull=False))
+    if f.qs:
+        table = MonitorTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='size', title='Monitors by Size and Price', x_label='Price ($)',
+                              y_label='Size (inches)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
 
-    table = MonitorTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='size', title='Monitors by Size and Price', x_label='Price ($)',
-                          y_label='Size (inches)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-
-    return render(request, 'dashboard_test.html', {
-        'title': 'Monitors',
-        'table_title': 'Monitors',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+        return render(request, 'dashboard_test.html', {
+            'title': 'Monitors',
+            'table_title': 'Monitors',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/monitor_graph')
 
 
 def cpu_graph(request):
     f = CPUFilter(request.GET, queryset=CPUSpecs.objects.filter(price__isnull=False))
-    table = CPUTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='core_clock', title='CPUS by Core Clock and Price', x_label='Price ($)',
-                          y_label='Core Clock (GHz)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'CPUs',
-        'table_title': 'CPUs',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = CPUTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='core_clock', title='CPUS by Core Clock and Price',
+                              x_label='Price ($)',
+                              y_label='Core Clock (GHz)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'CPUs',
+            'table_title': 'CPUs',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/cpu_graph')
 
 
 def gpu_graph(request):
     f = GPUFilter(request.GET, queryset=GPUSpecs.objects.filter(price__isnull=False))
-    table = GPUTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='memory', title='GPUs by Memory and Price', x_label='Price ($)',
-                          y_label='Memory (GB)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'GPUs',
-        'table_title': 'GPUs',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = GPUTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='memory', title='GPUs by Memory and Price', x_label='Price ($)',
+                              y_label='Memory (GB)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'GPUs',
+            'table_title': 'GPUs',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/gpu_graph')
 
 
 def motherboard_graph(request):
     f = MBFilter(request.GET, queryset=MBSpecs.objects.filter(price__isnull=False))
-    table = MBTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='mem_max', title='Motherboards by Memory Max and Price',
-                          x_label='Price ($)',
-                          y_label='Memory Maximum (GB)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'MotherBoards',
-        'table_title': 'MotherBoards',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = MBTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='mem_max', title='Motherboards by Memory Max and Price',
+                              x_label='Price ($)',
+                              y_label='Memory Maximum (GB)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'MotherBoards',
+            'table_title': 'MotherBoards',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/motherboard_graph')
 
 
 def psu_graph(request):
     f = PSUFilter(request.GET, queryset=PSUSpecs.objects.filter(price__isnull=False))
-
-    table = PSUTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='wattage', title='PSUs by Wattage and Price',
-                          x_label='Price ($)',
-                          y_label='Watts (W)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'PSUs',
-        'table_title': 'PSUs',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = PSUTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='wattage', title='PSUs by Wattage and Price',
+                              x_label='Price ($)',
+                              y_label='Watts (W)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'PSUs',
+            'table_title': 'PSUs',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/psu_graph')
 
 
 def ram_graph(request):
     qs = RAMSpecs.objects.filter(price_per_gb__isnull=False)
     f = RAMFilter(request.GET, queryset=qs.filter(price__isnull=False))
-
-    table = RAMTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='price_per_gb', title='RAM by Price Per GB and Price',
-                          x_label='Price ($)',
-                          y_label='Price Per GB ($)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'RAM',
-        'table_title': 'RAMs',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = RAMTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='price_per_gb', title='RAM by Price Per GB and Price',
+                              x_label='Price ($)',
+                              y_label='Price Per GB ($)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'RAM',
+            'table_title': 'RAMs',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/ram_graph')
 
 
 def stor_graph(request):
     qs = StorSpecs.objects.filter(price_per_gb__isnull=False)
     f = StorFilter(request.GET, queryset=qs.filter(price__isnull=False))
-
-    table = StorTable(f.qs)
-    graph = CategoryGraph(f.qs, x='price', y='price_per_gb', title='Storage by Price Per GB and Price',
-                          x_label='Price ($)',
-                          y_label='Price Per GB ($)')
-    graph_html = graph.get_html_graph()
-    RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-    return render(request, 'dashboard_test.html', {
-        'title': 'Storage',
-        'table_title': 'Storage',
-        'table': table,
-        'filter': f,
-        'graph_html': graph_html})
+    if f.qs:
+        table = StorTable(f.qs)
+        graph = CategoryGraph(f.qs, x='price', y='price_per_gb', title='Storage by Price Per GB and Price',
+                              x_label='Price ($)',
+                              y_label='Price Per GB ($)')
+        graph_html = graph.get_html_graph()
+        RequestConfig(request).configure(table)
+        table.paginate(page=request.GET.get("page", 1), per_page=25)
+        return render(request, 'dashboard_test.html', {
+            'title': 'Storage',
+            'table_title': 'Storage',
+            'table': table,
+            'filter': f,
+            'graph_html': graph_html})
+    else:
+        messages.error(request, 'No products found with selected filter options please try a broader search')
+        return redirect('/stor_graph')
 
 
 def login_register(request):
@@ -171,11 +196,10 @@ def login_register(request):
         else:
             return redirect('/')
     else:
-        messages.info(request,'Enter Login Information')
+        messages.info(request, 'Enter Login Information')
         return redirect('/')
+
 
 def logout(request):
     auth.logout(request)
     return redirect('/')
-
-
